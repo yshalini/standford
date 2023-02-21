e0bfc9bf7359c2992f121e710547639e539013fb
Shalini <shaliniy@boston-technology.com>
2023-02-21 12:02:49 +0530

    password delete

--- pageObjects/loginpage.py
+++ pageObjects/loginpage.py
@@ -3,11 +3,11 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
     email= (By.ID,"email")
-    password=(By.ID,"password")
+    # password=(By.ID,"password")
     signin=(By.ID,"loginBtnId")
     def getEmail(self):
             return self.driver.find_element(*LoginPage.email)
-    def getPassword(self):
-            return self.driver.find_element(*LoginPage.password)
+    # def getPassword(self):
+    #         return self.driver.find_element(*LoginPage.password)
     def getSignin(self):
             return self.driver.find_element(*LoginPage.signin)
